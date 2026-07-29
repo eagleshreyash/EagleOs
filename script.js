1e093740b3b8fabaf9342d463332e80baabec5f5
eagleshreyash <135635045+eagleshreyash@users.noreply.github.com>
2026-07-29 18:54:01 +0530

    plz work

--- script.js
+++ script.js
@@ -40,16 +40,9 @@ function dragElement(elmnt) {
     document.onmousemove = null;
   }
 }
-var welcomeScreen = document.querySelector("#mydiv")
-function closeWindow(element) {
-  element.style.display = "none"
-}
-function openWindow(element) {
-  element.style.display = "flex"
-}
-var welcomeScreenClose = document.querySelector("#mydivclose")
+var welcomeScreenClose = document.querySelector("#welcomeclose")
 
-var welcomeScreenOpen = document.querySelector("#mydivopen")
+var welcomeScreenOpen = document.querySelector("#welcomeopen")
 welcomeScreenClose.addEventListener("click", function() {
   closeWindow(welcomeScreen);
 });
@@ -57,3 +50,4 @@ welcomeScreenClose.addEventListener("click", function() {
 welcomeScreenOpen.addEventListener("click", function() {
   openWindow(welcomeScreen);
 });
+
